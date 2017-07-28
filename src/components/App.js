55138b8c7b549197014ff6245c396c1a20b5279c
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-28 10:32:16 -0400

    added hint text from project notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import EarthquakeList from './EarthquakeList';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div className="title">
-      <div className="my-header">
-      Earthquakes!
-      </div>
-      </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {EarthquakeList}
+
       </div>
     );
   }
