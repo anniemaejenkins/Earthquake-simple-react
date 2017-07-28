b8120d2db16ff5bf48d29b5f842753a3bba3122f
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-28 11:06:50 -0400

    mapped over data and assigned it to card div

--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
 import moment from 'moment';
 
-import Earthquakes from './../Earthquakes.js'
+import earthquakes from './../data/Earthquakes.js'
+import EarthquakeInfo from './EarthquakeInfo';
 
 
 export default class EarthquakeList extends Component{
   render(){
-    return(
-      <div className="col-sm-6" key={FILL_ME_IN_WITH_A_UNIQUE_KEY}>
-        <div className="card" >
-          <div className="card-block">
-            <h4 className="card-title">{FILL_ME_IN_WITH_THE_PLACE}</h4>
-            <h6 className="card-subtitle mb-2 text-muted">Magnitude: {FILL_ME_IN_WITH_THE_MAGNITUDE_MAG}</h6>
-            <h6 className="card-subtitle mb-2 text-muted">Time: {moment(FILL_ME_IN_WITH_THE_TIME).format('llll')}</h6>
-            <p className="card-text">Coordinates: {FILL_ME_IN_WITH_THE_COORDINATES}</p>
-            <a href={FILL_ME_IN_WITH_THE_URL} className="card-link">USGS Event Link</a>
+    let earthQuakes = earthquakes.features.map((earthquake)=>{
+      return(
+        <div className="col-sm-6" key={earthquake.id}>
+          <div className="card" >
+            <div className="card-block">
+              <h4 className="card-title">{earthquake.features.properties.place}</h4>
+              <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquake.features.properties.mag}</h6>
+              <h6 className="card-subtitle mb-2 text-muted">Time: {moment(earthquake.features.properties.time).format('llll')}</h6>
+              <p className="card-text">Coordinates: {earthquake.features.geometry.coordinates}</p>
+              <a href={earthquake.features.properties.url} className="card-link">USGS Event Link</a>
+            </div>
           </div>
         </div>
+      )
+    });
+
+    return(
+      <div>
+        <EarthquakeInfo />
+        {earthQuakes}
+
       </div>
     )
   }
